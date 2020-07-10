2b07df26537e3f600b1c1510e755a20ea656bc08
Tian Qin <tianqin@berkeley.edu>
2020-07-09 20:34:32 -0700

    Updates

---
+++ precache-manifest.3d61a4b51729e9f358d8fca2d299952a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcaa803eb847dfe77be0f4033d960d93",
+    "url": "/spockqin/tianqin.me/index.html"
+  },
+  {
+    "revision": "2d3efee25871f1dd1696",
+    "url": "/spockqin/tianqin.me/static/css/main.de06a343.chunk.css"
+  },
+  {
+    "revision": "9224668a9980d5dc7ed5",
+    "url": "/spockqin/tianqin.me/static/js/2.c4928dcd.chunk.js"
+  },
+  {
+    "revision": "0f544458a968df1596e861027de46f92",
+    "url": "/spockqin/tianqin.me/static/js/2.c4928dcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d3efee25871f1dd1696",
+    "url": "/spockqin/tianqin.me/static/js/main.afe282eb.chunk.js"
+  },
+  {
+    "revision": "5875eb80ea990feb98b1",
+    "url": "/spockqin/tianqin.me/static/js/runtime-main.d61fb418.js"
+  },
+  {
+    "revision": "a404a60d09c6cd8528e839f3fc20438b",
+    "url": "/spockqin/tianqin.me/static/media/portrait.a404a60d.jpg"
+  }
+]);
